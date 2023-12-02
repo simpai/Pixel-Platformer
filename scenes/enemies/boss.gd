6b55630178a5f3ddf1b1635e75552a7d84fdf130
simpai <junghun.cha@gmail.com>
2023-12-02 12:19:01 +0900

    boss ai

--- scenes/enemies/boss.gd
+++ scenes/enemies/boss.gd
@@ -16,55 +16,73 @@ var moveTime:float = 0
 var moveLeft:bool = true
 
 func get_random_move_time():
-	return randf_range(1,4)
+	return randf_range(1,2)
 
 func _ready():
-	move_start(get_random_move_time(), true)
+	move_start(true)
 
 
 func _physics_process(delta):
-	move_state(delta)
+	match state:
+		STATE.MOVE: move_state(delta)
+		STATE.JUMP: jump_state(delta)
+		STATE.ATTACK: attack_state(delta)
 
-func move_start(move_time:float, left:bool):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	move_and_slide()
+		
+func move_start(left:bool):
+	print("move_start")
 	state = STATE.MOVE
-	moveTime = move_time
+	moveTime = get_random_move_time()
 	moveLeft = left
 	animation_player.play("walking")
 
+func jump_start():
+	print("jump_start")
+	state = STATE.JUMP
+	animation_player.play("jump")
+	velocity.y = -500
+	velocity.x *= 3
+
+func attack_start():
+	print("attack_start")
+	state = STATE.ATTACK
+	velocity.x = 0
+	animation_player.play("spike")
+	
 func move_state(delta):
 	moveTime -= delta
 	if moveTime <= 0:
-		move_start(get_random_move_time(), !moveLeft)
+		move_start(moveLeft)
 	
 	if moveLeft:
 		velocity.x = -SPEED
 	else:
 		velocity.x = SPEED
 
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	move_and_slide()
-
-var attacking = false
-func _on_jump_timer_timeout():
-	if attacking == false:
-		velocity.y = -500
-		animation_player.play("jump")
-		await animation_player.animation_finished
-	animation_player.play("walking")
-
-
-func _on_attack_timer_timeout():
-	attacking = true
-	animation_player.play("spike")
-	await animation_player.animation_finished
-	attacking = false
-	animation_player.play("walking")
+func jump_state(delta):
+	if is_on_floor():
+		move_start(!moveLeft)
 	
-	for i in range(6):
-		var force = randf_range(400,500)
-		var _velocity = Vector2(-force, 0).rotated(deg_to_rad(i*30))
-		var bomb = bombScene.instantiate()
-		get_parent().add_child(bomb)
-		bomb.setBomb(position, _velocity, 3)
+func attack_state(_delta):
+	if animation_player.is_playing() == false:
+		for i in range(6):
+			var force = randf_range(400,500)
+			var _velocity = Vector2(-force, 0).rotated(deg_to_rad(i*30))
+			var bomb = bombScene.instantiate()
+			get_parent().add_child(bomb)
+			bomb.setBomb(position, _velocity, 3)
+		move_start(!moveLeft)
+		
+func _on_ai_timer_timeout():
+	select_action()
+		
+func select_action():
+	match randi_range(0,2):
+		0: move_start(!moveLeft)
+		1: attack_start()
+		2: jump_start()
